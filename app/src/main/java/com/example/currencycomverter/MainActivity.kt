19b1f7a0e51406733f43129ae8264c93ed9abe0e
THE-ASHUTOSH <ashubham1906@gmail.com>
2024-11-22 22:10:34 +0530

    Final

--- app/src/main/java/com/example/currencycomverter/MainActivity.kt
+++ app/src/main/java/com/example/currencycomverter/MainActivity.kt
@@ -3,21 +3,27 @@ package com.example.currencycomverter
 import android.icu.util.Currency
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Vertices
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,17 +89,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CurrencyComverterTheme {
 
+                val col = Color(red = 255, green = 100, blue = 50)
                 var fromCurrency by remember { mutableStateOf("From Value") }
                 var toCurrency by remember { mutableStateOf("To value") }
                 var fromCurrencyV by remember { mutableStateOf("") }
                 var toCurrencyV by remember { mutableStateOf("") }
                 var convertedValue by remember {
-                    mutableStateOf("Converted Value")
+                    mutableStateOf("")
                 }
                 var price by remember { mutableStateOf("") }
-
+                fun swap(){
+                    val temp = fromCurrency;
+                    fromCurrency = toCurrency;
+                    toCurrency = temp;
+                    val temp2 = fromCurrencyV;
+                    fromCurrencyV = toCurrencyV;
+                    toCurrencyV = temp2;
+                }
 
                 fun handleCheckClicked(viewModel: ExchangeRateViewModel = ExchangeRateViewModel()) {
                     val exchangeRates by viewModel.exchangeRates
@@ -101,61 +114,98 @@ class MainActivity : ComponentActivity() {
 
                     if (inputPrice == null || fromCurrency == "From Value" || toCurrency ==
                         "To value") {
-                        convertedValue = "Invalid input! Please enter a valid number and select currencies."
-                        return
+                        Toast.makeText(this, "Invalid input! Please enter a valid number and select currencies.", Toast.LENGTH_SHORT).show()
+
                     }else {
 
                         val fromRate = fromCurrencyV.toFloat()
                         val toRate = toCurrencyV.toFloat()
-                        val FromRate = "From Rate: %.2f".format(fromRate)
-                        val ToRate = "To Rate: %.2f".format(toRate)
+                        val FromRate = "From Rate: %.3f".format(fromRate)
+                        val ToRate = "To Rate: %.3f".format(toRate)
 
                         val result = (toRate / fromRate) * inputPrice
-                        convertedValue = "Converted Value: %.2f".format(result)
+                        convertedValue = "%.3f".format(result)
                     }
                 }
 //                converted = calculate(fromCurrency,toCurrency)
                 Column(modifier = Modifier
-                    .background(Color.LightGray)
                     .fillMaxSize()
-                    .padding(10.dp)) {
+                    .padding(10.dp)
+                    ) {
+                    
                     Text(text = "Currency Converter",
                         fontSize = 30.sp,
                         modifier = Modifier
                             .align(alignment = Alignment.CenterHorizontally)
+                            .clip(RoundedCornerShape(16.dp))
+                            .border(1.dp, Color.Black, shape = RoundedCornerShape(16.dp))
                             .padding(20.dp),
                     )
                     inputField(
                         price = price,
                         onPriceChange = { price = it },
-                        onCheckClicked = { handleCheckClicked() }
-                    )
+                        onCheckClicked = { handleCheckClicked() },)
                     Text(text = "From",
-                        modifier = Modifier.padding(horizontal = 10.dp))
-                    Box(modifier = Modifier.border(2.dp,Color.Gray)){
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp, vertical = 10.dp),
+                        color = Color(0xFFFF6432))
+                    Box(modifier = Modifier.border(2.dp,Color.Black)){
                         DropdownExample(selectedOption = fromCurrency,
                             onOptionSelected = { selected -> fromCurrency = selected },
                             onValueSelected = {s -> fromCurrencyV = s.toString()})
                     }
+
+                    Column(modifier = Modifier.fillMaxWidth()){
+                        Button(onClick = {swap()
+                                         handleCheckClicked()},
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .clip(RoundedCornerShape(100.dp))
+                                .padding(top = 20.dp, bottom = 10.dp),
+
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent,
+                                contentColor = Color.Black),
+                            border = BorderStroke(2.dp,Color(0xFFFF6432)),
+                        ) {
+                            Text(text = "↑↓",
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.ExtraBold )
+                        }
+                    }
                     Text(text = "To",
-                        modifier = Modifier.padding(horizontal = 10.dp))
-                    Box(modifier = Modifier.border(2.dp,Color.Gray)){
+                        modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp),
+                        color = Color(0xFFFF6432))
+                    Box(modifier = Modifier.border(2.dp,Color.Black)){
                         DropdownExample( selectedOption = toCurrency,
                             onOptionSelected = { selected -> toCurrency = selected },
                             onValueSelected = {s -> toCurrencyV = s.toString()})
                     }
-                    Box(modifier = Modifier
-                        .border(1.dp, color = Color.Black)
-                        .align(Alignment.CenterHorizontally)){
-                        Text(text = "$convertedValue",
-                            modifier = Modifier.padding(10.dp))
+                    Column(modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(20.dp)) {
+                        Box(modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+
+                            .border(1.dp, Color.Black, shape = RoundedCornerShape(16.dp))){
+                            Row(modifier = Modifier.padding(10.dp)) {
+                                Text(text = "Converted Value : ",
+                                    fontSize = 20.sp
+                                )
+                                Text(text = "$convertedValue",
+                                    fontSize = 20.sp,
+                                    color = Color(0xFFFF6432))
+                            }
+
+                        }
                     }
+
                 }
 
             }
         }
     }
-}
+
 
 // UI Composables
 //@Composable
@@ -222,7 +272,7 @@ fun DropdownExample(selectedOption: String,
             text = selectedOption,
             modifier = Modifier
 
-                .padding(16.dp)
+                .padding(horizontal = 16.dp, vertical = 5.dp)
                 .clickable { expanded = true } // Open dropdown on click
                 .weight(0.7F)
 
@@ -232,7 +282,7 @@ fun DropdownExample(selectedOption: String,
             imageVector = Icons.Default.ArrowDropDown,
             contentDescription = "Arrow Icon",
             modifier = Modifier
-                .padding(16.dp)
+                .padding(horizontal = 16.dp, vertical = 5.dp)
                 .clickable { expanded = true } // Open dropdown on click
                 .weight(0.3F)
         )
@@ -257,7 +307,8 @@ fun DropdownExample(selectedOption: String,
                     },
                     text = { Text(option.key, color = Color.Black,
                         textAlign = TextAlign.Center)
-                        HorizontalDivider() },
+                        HorizontalDivider(color=Color(red = 255, green = 100, blue = 50),
+                            thickness = 1.dp) },
 
                     )
             }
@@ -279,12 +330,26 @@ fun inputField(
         Row {
             OutlinedTextField(
                 value = price,
+                placeholder = { Text(text = " Enter Amount") },
                 onValueChange = { text -> onPriceChange(text) },
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 10.dp),
+
             )
             Spacer(modifier = Modifier.padding(10.dp))
-            Button(onClick = { onCheckClicked() }) {
-                Text(text = "Check")
+            val col = Color(red = 255, green = 100, blue = 50)
+            Button(onClick = { onCheckClicked() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = col),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .padding( vertical = 10.dp)
+
+                    ,
+            ) {
+                Text(text = "Convert",
+                    modifier = Modifier.padding(4.dp))
             }
         }
     }
@@ -310,5 +375,6 @@ fun inputField(
 fun CurrencyConverterAppPreview() {
     CurrencyComverterTheme{
 //        CurrencyConverterApp()
+
     }
 }
